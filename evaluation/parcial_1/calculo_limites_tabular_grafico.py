3909ed90872cfba031404a553301f0bbfe116aa6
Marco Julio Cañas Campillo <66282835+marco-canas@users.noreply.github.com>
2023-03-26 20:37:29 -0500

    adición de parcial 1 tema 3 sobre enfasis en lectura de tables y gráficos para la determinación de límites

---
+++ evaluation/parcial_1/calculo_limites_tabular_grafico.py
@@ -0,0 +1,4 @@
+Python 3.10.10 (tags/v3.10.10:aad5f6a, Feb  7 2023, 17:20:36) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import numpy as np
+impo
---
+++ evaluation/parcial_1/modulo_calculo_limites_tabular_grafico.py
@@ -0,0 +1,9 @@
+import numpy as np 
+import pandas as pd 
+
+def f(x):
+    
+
+def tabla_izquierda(f, c = 0):
+    X = np.linspace(c-1, c)
+    Y = f(x)
