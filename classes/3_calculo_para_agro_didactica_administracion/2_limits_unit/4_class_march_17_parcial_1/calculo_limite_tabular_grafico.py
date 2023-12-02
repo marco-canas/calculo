03d2aebc9a71c5b18c3efe14c27b455aad415b23
Marco Cañas <marco.canas@udea.edu.co>
2023-12-02 09:37:59 -0500

    tres ejes de formación para el cálculo en el campus caucasia

---
+++ classes/3_calculo_para_agro_didactica_administracion/2_limits_unit/4_class_march_17_parcial_1/calculo_limite_tabular_grafico.py
@@ -0,0 +1,45 @@
+import numpy as np 
+import pandas as pd 
+
+    
+
+def tabla_izquierda(f, c = 0):
+    epsilon = 0.001
+    X = np.linspace(c-1, c-epsilon, 10)
+    Y = f(X)
+    tabla_izq = pd.DataFrame({'x':X, 'y':Y})
+    return tabla_izq
+
+
+def tabla_derecha(f, c = 0):
+    epsilon = 0.001
+    X = np.linspace(c+epsilon, c+1-epsilon, 10)
+    Y = f(X)
+    tabla_der = pd.DataFrame({'x':X, 'y':Y})
+    return tabla_der
+
+def grafico_de_dispersion():
+    import pandas as pd 
+    import plotly.express as px
+    #fig = px.line(tabla,x = tabla.keys()[0], y = tabla.keys()[1], title=r'Relación entre $x$ y $y$')
+    fig = px.scatter(tabla,x = tabla.keys()[0], y = tabla.keys()[1], title=r'Relación entre $x$ y $y$')
+    fig.show()
+    
+    
+def trazar_flecha(punto_inicial = [0,0], delta_x = 3, delta_y = 4, **opcions):
+    return plt.arrow()
+
+def plot_tendencia_funcion(c = 2):
+    punto_final_flechas = [c, f(c)]
+    epsilon = 0.000001
+    derivada_en_c = (f(c+epsilon) - f(c-epsilon))/(2*epsilon)
+    # flecha superior
+    plt.arrow(c + 1.2,c + 6.4, -1, -derivada_en_c, head_width=0.1)
+    
+    # flecha inferior
+    plt.arrow(c-1, c-2, 1, derivada_en_c, head_width=0.1) 
+    #plt.arrow()
+
+
+if __name__=='__main__':
+    pass 
