ad33908a6213af5eef280abad6c30c4dc3b4e6d3
Marco Cañas <marco.canas@udea.edu.co>
2024-07-30 21:01:38 -0500

    mejoramos el programa de graficación de la función diferencia de los lados ingresada por el usuario

--- referents/1_precalculo_stewart/1_fundamentos/1_5_ecuaciones/6_propuesta_presentada_congreso_humanos_XXI/5_ensayo_de_construccion_aplicacion_android_para_obtencion_representacion_grafica/graficas_funcion.py
+++ referents/1_precalculo_stewart/1_fundamentos/1_5_ecuaciones/6_propuesta_presentada_congreso_humanos_XXI/5_ensayo_de_construccion_aplicacion_android_para_obtencion_representacion_grafica/graficas_funcion.py
@@ -2,10 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Solicitar al usuario la función matemática
-func_str = input("Ingrese la función matemática en términos de x (por ejemplo, np.sin(x)): ")
+func_str = input("""Ingrese la función diferencia 
+                 de los lados de la ecuación 
+                 en términos de x (por ejemplo, np.sin(x)): """)
 
 # Crear el conjunto de puntos x
-x = np.arange(start = -10, stop = 10, step = 0.5)
+a,b = -10,10 # definir el dominio de graficación
+x = np.arange(start = a, stop = b, step = 0.5)
 
 try:
     # Evaluar la función ingresada por el usuario
@@ -13,6 +16,7 @@ try:
 
     # Graficar la función
     plt.plot(x, y, 'r--')
+    plt.axhline(y = 0, xmin = a, xmax = b) 
     plt.title(f'Grafica de la función: {func_str}')
     plt.xlabel('x')
     plt.ylabel('y')
